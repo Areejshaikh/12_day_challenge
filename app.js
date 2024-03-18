028c21e421833e4555d44a0a6a08eb10d6ecde45
Areejshaikh <160318564+Areejshaikh@users.noreply.github.com>
2024-03-19 02:47:19 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+// // Day 12 Challenge: Start Coding!
+// // Question 34:
+// const pizzas: string[] = ["BBQ Chicken", "Spicy Beef", "Vegetable"]; //Array
+// pizzas.forEach((pizza) => {
+//   console.log(`Is my favrite ${pizza}`);
+// });
+// console.log("i really like to pizza");
+// //Question 35:
+// const animals: string[] = ["Cow", "Cat", "Dog"];
+// animals.forEach((animal) => {
+//   console.log(`I really like to this ${animals}`);
+// });
+// console.log("i like to this pets");
+// //Question 36:
+// function shirt(size: string , message: string) {
+//     console.log(`Making a ${size} t-shirt with the message "${message}" printed on it.`);
+// };
+function make_shirt(size, message) {
+    console.log("Making a ".concat(size, " t-shirt with the message \"").concat(message, "\" printed on it."));
+}
+make_shirt("medium", "Code is Life");
